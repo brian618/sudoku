6715360ed4bb0f98465a7700fb9a3713941fe96f
brian618 <brianc-@live.ca>
2015-11-24 14:56:59 -0800

    Added comments for field

--- src/com/roastlechon/games/sudoku/view/Field.java
+++ src/com/roastlechon/games/sudoku/view/Field.java
@@ -14,74 +14,91 @@ import javax.swing.text.MaskFormatter;
 import com.roastlechon.games.sudoku.model.Square;
 
 public class Field extends JFormattedTextField {
-	
-	private static final long serialVersionUID = 8031599756854199153L;
-	
-	Border border = BorderFactory.createEmptyBorder();
-	Square square;
-	
-	public Field() {
-		super(getMaskFormatter());
-		Dimension dimension = new Dimension(20, 20);
-		this.setPreferredSize(dimension);
-		this.setHorizontalAlignment(JFormattedTextField.CENTER);
-		this.setFocusable(false);
-		this.setEditable(false);
-		this.setBackground(new Color(240, 240, 240));
-		this.setBorder(border);
-	}
-	
-	public Field(Square square) {
-		super(getMaskFormatter());
-		
-		this.square = square;
-		Dimension dimension = new Dimension(20, 20);
-		this.setPreferredSize(dimension);
-		this.setHorizontalAlignment(JFormattedTextField.CENTER);
-		
-		if(square.value == 0) {
-			this.setValue(null);
-			this.setBorder(border);
-			this.addKeyListener(new KeyListener() {
 
-				public void keyPressed(KeyEvent arg0) {
-					
-				}
+    private static final long serialVersionUID = 8031599756854199153L;
 
-				public void keyReleased(KeyEvent arg0) {
-					setCaretPosition(0);
-				}
+    Border border = BorderFactory.createEmptyBorder();
+    Square square;
 
-				public void keyTyped(KeyEvent arg0) {
-					setCaretPosition(0);
-					transferFocus();
-				}
-				
-			});
-		} else {
-			this.setValue(String.valueOf(square.value));
-			this.setFocusable(false);
-			this.setEditable(false);
-			this.setBackground(new Color(240, 240, 240));
-			this.setBorder(border);
+    /**
+     * Constructor to create a new empty field
+     */
+    public Field() {
+	super(getMaskFormatter());
+	Dimension dimension = new Dimension(20, 20);
+	this.setPreferredSize(dimension);
+	this.setHorizontalAlignment(JFormattedTextField.CENTER);
+	this.setFocusable(false);
+	this.setEditable(false);
+	this.setBackground(new Color(240, 240, 240));
+	this.setBorder(border);
+    }
+
+    /**
+     * Create a field that represents a space on the UI
+     * 
+     * @param square,
+     *            the Square that is represented by the field
+     */
+    public Field(Square square) {
+	super(getMaskFormatter());
+
+	this.square = square;
+	Dimension dimension = new Dimension(20, 20);
+	this.setPreferredSize(dimension);
+	this.setHorizontalAlignment(JFormattedTextField.CENTER);
+
+	if (square.value == 0) {
+	    this.setValue(null);
+	    this.setBorder(border);
+	    this.addKeyListener(new KeyListener() {
+
+		public void keyPressed(KeyEvent arg0) {
+
 		}
-	}
-	
-	public static MaskFormatter getMaskFormatter() {
-		MaskFormatter fieldFormat = null;
-		try {
-			fieldFormat  = new MaskFormatter("*");
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+		public void keyReleased(KeyEvent arg0) {
+		    setCaretPosition(0);
 		}
-		fieldFormat.setValidCharacters("123456789 ");
-		fieldFormat.setInvalidCharacters("0");
-		return fieldFormat;
+
+		public void keyTyped(KeyEvent arg0) {
+		    setCaretPosition(0);
+		    transferFocus();
+		}
+
+	    });
+	} else {
+	    this.setValue(String.valueOf(square.value));
+	    this.setFocusable(false);
+	    this.setEditable(false);
+	    this.setBackground(new Color(240, 240, 240));
+	    this.setBorder(border);
 	}
-	
-	public Square getSquare() {
-		return this.square;
+    }
+
+    /**
+     * formats acceptable inputs for the field
+     * 
+     * @return MaskFormatter with the valid characters
+     */
+    public static MaskFormatter getMaskFormatter() {
+	MaskFormatter fieldFormat = null;
+	try {
+	    fieldFormat = new MaskFormatter("*");
+	} catch (ParseException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
+	fieldFormat.setValidCharacters("123456789 ");
+	fieldFormat.setInvalidCharacters("0");
+	return fieldFormat;
+    }
+
+    /**
+     * @return the square contained in this field
+     */
+    public Square getSquare() {
+	return this.square;
+    }
 
 }
