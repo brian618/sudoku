5902891b0da092080c52d6ffaefa4a744ffd7aa1
brian618 <brianc-@live.ca>
2015-11-24 15:26:51 -0800

    Added comments to Sudoku

--- src/com/roastlechon/games/sudoku/Sudoku.java
+++ src/com/roastlechon/games/sudoku/Sudoku.java
@@ -17,148 +17,147 @@ import com.roastlechon.games.sudoku.view.Board;
 
 public class Sudoku extends JFrame {
 
-	private static final long serialVersionUID = 9179949591758990253L;
-	final JButton generateButton = new JButton("Generate");
-	final JButton finishButton = new JButton("Finish");
-	JFormattedTextField timerField = new JFormattedTextField();
-	final JLabel timerLabel = new JLabel("Time Elapsed");
-	JPanel panel = new JPanel();
-	Board board = new Board();
-	int delay = 1000;
-	int time = 1;
-	
-	final JButton easyButton = new JButton("E");
-	final JButton mediumButton = new JButton("M");
-	final JButton hardButton = new JButton("H");
-	
-	final JFrame messageDialog = new JFrame();
-		
-	Timer timer;
-	
-	public Sudoku() {
-		timerLabel.setLayout(null);
-		timerLabel.setBounds(10, 200, 80, 20);
-		
-		timerField.setHorizontalAlignment(JFormattedTextField.CENTER);
-		timerField.setLayout(null);
-		timerField.setEnabled(true);
-		timerField.setEditable(false);
-		timerField.setBounds(10, 225, 60, 20);
-		timerField.setValue("0");
-		timerField.setForeground(Color.BLACK);
-		
-		timer = new Timer(delay, new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-		          timerField.setValue(time++);
-			}
-		});
-		
-		easyButton.setLayout(null);
-		easyButton.setBounds(100,200,30,20);
-		easyButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				finishButton.setEnabled(true);
-				panel.remove(board);
-				board = new Board(new GenerationAlgorithm().puzzle);
-				panel.add(board);
-				panel.revalidate();
-				panel.repaint();
-				timerField.setValue("0");
-				time = 1;
-				timer.restart();
-				transferFocus();
-			}
-		});
-		
-		mediumButton.setLayout(null);
-		mediumButton.setBounds(130,200,30,20);
-		mediumButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				finishButton.setEnabled(true);
-				panel.remove(board);
-				board = new Board(new GenerationAlgorithm().puzzle);
-				panel.add(board);
-				panel.revalidate();
-				panel.repaint();
-				timerField.setValue("0");
-				time = 1;
-				timer.restart();
-				transferFocus();
-			}
-		});
-		hardButton.setLayout(null);
-		hardButton.setBounds(160, 200, 30, 20);
-		hardButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				finishButton.setEnabled(true);
-				panel.remove(board);
-				board = new Board(new GenerationAlgorithm().puzzle);
-				panel.add(board);
-				panel.revalidate();
-				panel.repaint();
-				timerField.setValue("0");
-				time = 1;
-				timer.restart();
-				transferFocus();
-			}
-		});
-		
-		/*generateButton.setLayout(null);
-		generateButton.setBounds(100, 200, 45, 20);
-		generateButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				finishButton.setEnabled(true);
-				panel.remove(board);
-				board = new Board(new GenerationAlgorithm().puzzle);
-				panel.add(board);
-				panel.revalidate();
-				panel.repaint();
-				timerField.setValue("0");
-				time = 1;
-				timer.restart();
-				transferFocus();
-			}
-		});*/
-		
-		finishButton.setLayout(null);
-		finishButton.setBounds(100, 225, 90, 20);
-		finishButton.setEnabled(false);
-		finishButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (board.isValid()) {
-					if (board.isComplete()) {
-						finishButton.setEnabled(false);
-						timer.stop();
-						JOptionPane.showMessageDialog(messageDialog, "Congratulations, you finished the puzzle in " + timerField.getValue() + " seconds. Click Generate to play again.");
-					} else {
-						JOptionPane.showMessageDialog(messageDialog, "The sudoku puzzle is not complete. Please check the puzzle and click Finish.");
-					}
-				} else {
-					JOptionPane.showMessageDialog(messageDialog, "The sudoku puzzle is not complete. Please check the puzzle and click Finish.");
-				}
-			}
-		});
-		
-		panel.setLayout(null);
-		panel.add(timerLabel);
-		panel.add(timerField);
+    private static final long serialVersionUID = 9179949591758990253L;
+    final JButton generateButton = new JButton("Generate");
+    final JButton finishButton = new JButton("Finish");
+    JFormattedTextField timerField = new JFormattedTextField();
+    final JLabel timerLabel = new JLabel("Time Elapsed");
+    JPanel panel = new JPanel();
+    Board board = new Board();
+    int delay = 1000;
+    int time = 1;
+
+    final JButton easyButton = new JButton("E");
+    final JButton mediumButton = new JButton("M");
+    final JButton hardButton = new JButton("H");
+
+    final JFrame messageDialog = new JFrame();
+
+    Timer timer;
+
+    /**
+     * Constructor for Sudoku
+     */
+    public Sudoku() {
+	timerLabel.setLayout(null);
+	timerLabel.setBounds(10, 200, 80, 20);
+
+	timerField.setHorizontalAlignment(JFormattedTextField.CENTER);
+	timerField.setLayout(null);
+	timerField.setEnabled(true);
+	timerField.setEditable(false);
+	timerField.setBounds(10, 225, 60, 20);
+	timerField.setValue("0");
+	timerField.setForeground(Color.BLACK);
+
+	timer = new Timer(delay, new ActionListener() {
+	    public void actionPerformed(ActionEvent evt) {
+		timerField.setValue(time++);
+	    }
+	});
+
+	easyButton.setLayout(null);
+	easyButton.setBounds(100, 200, 30, 20);
+	easyButton.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent e) {
+		finishButton.setEnabled(true);
+		panel.remove(board);
+		board = new Board(new GenerationAlgorithm().puzzle);
 		panel.add(board);
-		//panel.add(generateButton);
-		panel.add(mediumButton);
-		panel.add(hardButton);
-		panel.add(easyButton);
-		panel.add(finishButton);
-		
-		this.add(panel);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setSize(204, 285);
-		this.setLocationRelativeTo(null);
-		this.setTitle("Sudoku");
-		this.setResizable(false);
-		this.setVisible(true);
-	}
-	
-	public static void main(String[] args) {
-		new Sudoku();
-	}
+		panel.revalidate();
+		panel.repaint();
+		timerField.setValue("0");
+		time = 1;
+		timer.restart();
+		transferFocus();
+	    }
+	});
+
+	mediumButton.setLayout(null);
+	mediumButton.setBounds(130, 200, 30, 20);
+	mediumButton.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent e) {
+		finishButton.setEnabled(true);
+		panel.remove(board);
+		board = new Board(new GenerationAlgorithm().puzzle);
+		panel.add(board);
+		panel.revalidate();
+		panel.repaint();
+		timerField.setValue("0");
+		time = 1;
+		timer.restart();
+		transferFocus();
+	    }
+	});
+	hardButton.setLayout(null);
+	hardButton.setBounds(160, 200, 30, 20);
+	hardButton.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent e) {
+		finishButton.setEnabled(true);
+		panel.remove(board);
+		board = new Board(new GenerationAlgorithm().puzzle);
+		panel.add(board);
+		panel.revalidate();
+		panel.repaint();
+		timerField.setValue("0");
+		time = 1;
+		timer.restart();
+		transferFocus();
+	    }
+	});
+
+	/*
+	 * generateButton.setLayout(null); generateButton.setBounds(100, 200,
+	 * 45, 20); generateButton.addActionListener(new ActionListener() {
+	 * public void actionPerformed(ActionEvent e) {
+	 * finishButton.setEnabled(true); panel.remove(board); board = new
+	 * Board(new GenerationAlgorithm().puzzle); panel.add(board);
+	 * panel.revalidate(); panel.repaint(); timerField.setValue("0"); time =
+	 * 1; timer.restart(); transferFocus(); } });
+	 */
+
+	finishButton.setLayout(null);
+	finishButton.setBounds(100, 225, 90, 20);
+	finishButton.setEnabled(false);
+	finishButton.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent e) {
+		if (board.isValid()) {
+		    if (board.isComplete()) {
+			finishButton.setEnabled(false);
+			timer.stop();
+			JOptionPane.showMessageDialog(messageDialog, "Congratulations, you finished the puzzle in "
+				+ timerField.getValue() + " seconds. Click Generate to play again.");
+		    } else {
+			JOptionPane.showMessageDialog(messageDialog,
+				"The sudoku puzzle is not complete. Please check the puzzle and click Finish.");
+		    }
+		} else {
+		    JOptionPane.showMessageDialog(messageDialog,
+			    "The sudoku puzzle is not complete. Please check the puzzle and click Finish.");
+		}
+	    }
+	});
+
+	panel.setLayout(null);
+	panel.add(timerLabel);
+	panel.add(timerField);
+	panel.add(board);
+	// panel.add(generateButton);
+	panel.add(mediumButton);
+	panel.add(hardButton);
+	panel.add(easyButton);
+	panel.add(finishButton);
+
+	this.add(panel);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	this.setSize(204, 285);
+	this.setLocationRelativeTo(null);
+	this.setTitle("Sudoku");
+	this.setResizable(false);
+	this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+	new Sudoku();
+    }
 }
